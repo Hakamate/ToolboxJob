1ab7dc80b770514d398c1cd0471d832626e1c12f
Thomas CLEMENT <thomas.clement@viacesi.fr>
2021-09-15 09:20:03 +0200

    💄 check feature

--- pages/outils/_id.vue
+++ pages/outils/_id.vue
@@ -38,18 +38,20 @@
             </div> -->
             <div class="flex flex-col">
               <span class="text-sm font-medium text-gray-600">Fonctionnalités</span>
-              <div class="grid gap-1 mt-1">
-                <div v-for="(feature, indexFeature) in tool.fields['Fonctionnalités']" :key="indexFeature" class="text-sm w-auto flex items-center">
-                  <svg
-                    aria-hidden="true"
-                    focusable="false"
-                    data-prefix="fas"
-                    data-icon="check-circle"
-                    class="text-green-500 mx-1 w-3 h-3 svg-inline--fa fa-check-circle fa-w-16"
-                    role="img"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512"
-                  ><path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z" /></svg>
+              <div class="grid gap-2 mt-1">
+                <div v-for="(feature, indexFeature) in tool.fields['Fonctionnalités']" :key="indexFeature" class="text-sm w-auto flex">
+                  <div>
+                    <svg
+                      aria-hidden="true"
+                      focusable="false"
+                      data-prefix="fas"
+                      data-icon="check-circle"
+                      class="mt-1 w-auto text-green-500 mx-1 w-3 h-3 svg-inline--fa fa-check-circle fa-w-16"
+                      role="img"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 512 512"
+                    ><path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z" /></svg>
+                  </div>
                   <span>
                     {{ feature }}
                   </span>
