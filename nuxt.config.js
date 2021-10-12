c32023ca6babc0d441a8202431a516857ebe9a8f
Thomas CLEMENT <thomas.clement@viacesi.fr>
2021-10-12 11:41:04 +0200

    📝 Meta data

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,12 +10,25 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ToolboxJob',
+    title: 'ToolboxJob - La banque d\'outils pour booster votre recherche d\'emploi',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Tu es en recherche d\'emploi (Stage, Alternance, CDD, CDI, etc...) et tu cherches à améliorer tes résultats ? Bienvenue sur ToolboxJob une banque d\'outils, formations, newsletters (etc...) qui t\'accompagne dans ta quête du Graal !' },
+      { hid: 'author', name: 'author', content: 'Thomas CLEMENT' },
+      { hid: 'og:url', name: 'og:url', content: 'https://toolbox-job.vercel.app/' },
+      { hid: 'og:title', name: 'og:title', content: 'ToolboxJob - La banque d\'outils pour booster votre recherche d\'emploi' },
+      { hid: 'og:site_name', name: 'og:site_name', content: 'ToolboxJob - La banque d\'outils pour booster votre recherche d\'emploi' },
+      { hid: 'og:description', name: 'og:description', content: 'Tu es en recherche d\'emploi (Stage, Alternance, CDD, CDI, etc...) et tu cherches à améliorer tes résultats ? Bienvenue sur ToolboxJob une banque d\'outils, formations, newsletters (etc...) qui t\'accompagne dans ta quête du Graal !' },
+      { hid: 'og:image', name: 'og:image', content: 'https://toolbox-job.vercel.app/job_search.jpg' },
+      { hid: 'og:image:secure_url', name: 'og:image:secure_url', content: 'https://toolbox-job.vercel.app/job_search.jpg' },
+      { hid: 'og:type', name: 'og:type', content: 'website' },
+      { hid: 'og:image:type', name: 'og:image:type', content: 'image/jpg' },
+      { hid: 'og:image:alt', name: 'og:image:alt', content: 'ToolboxJob - La banque d\'outils pour booster votre recherche d\'emploi' },
+      { hid: 'twitter:site', name: 'twitter:site', content: 'https://toolbox-job.vercel.app/' },
+      { hid: 'twitter:title', name: 'twitter:title', content: 'ToolboxJob - La banque d\'outils pour booster votre recherche d\'emploi' },
+      { hid: 'twitter:description', name: 'twitter:description', content: 'Tu es en recherche d\'emploi (Stage, Alternance, CDD, CDI, etc...) et tu cherches à améliorer tes résultats ? Bienvenue sur ToolboxJob une banque d\'outils, formations, newsletters (etc...) qui t\'accompagne dans ta quête du Graal !' },
+      { hid: 'twitter:image', name: 'twitter:description', content: 'https://toolbox-job.vercel.app/job_search.jpg' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
